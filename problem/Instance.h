8d6bb59fb6a3969d9bd4ac0618151e0bd31fb16f
ADDALemos <alexandre.lemos@tecnico.ulisboa.pt>
2020-02-23 12:42:31 +0000

    TT-Open-WBO-Inc

---
+++ problem/Instance.h
@@ -0,0 +1,29 @@
+//
+// Created by Alexandre Lemos on 11/08/2018.
+//
+
+#ifndef TRAIN_SCHEDULE_OPTIMISATION_TIMETABLE_H
+#define TRAIN_SCHEDULE_OPTIMISATION_TIMETABLE_H
+#include <iostream>
+#include <list>
+#include <vector>
+#include <map>
+#include "Train.h"
+#include "Route.h"
+
+using namespace std;
+class Instance {
+public:
+    int hash;
+    std::string label;
+    std::string maxBandabweichung;
+    std::list<Train> train;
+    std::list<Route> route;
+    std::list<Resource> resource;
+
+
+    vector<map<string,vector<pair<route_section, route_section>>>> pair;
+};
+
+
+#endif //TRAIN_SCHEDULE_OPTIMISATION_TIMETABLE_H
