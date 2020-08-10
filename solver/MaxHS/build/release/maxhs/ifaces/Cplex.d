3900492fb03e4e165419588ba38c4f8af7f67866
ADDALemos <alexandre.lemos@tecnico.ulisboa.pt>
2020-08-10 19:50:15 +0100

    MaxHS solver added

---
+++ solver/MaxHS/build/release/maxhs/ifaces/Cplex.d
@@ -0,0 +1,12 @@
+build/release/maxhs/ifaces/Cplex.o: maxhs/ifaces/Cplex.cc \
+  minisat/utils/System.h minisat/mtl/IntTypes.h maxhs/ifaces/Cplex.h \
+  /Applications/CPLEX_Studio128/cplex/include/ilcplex/cplexx.h \
+  /Applications/CPLEX_Studio128/cplex/include/ilcplex/cpxconst.h \
+  /Applications/CPLEX_Studio128/cplex/include/ilcplex/cplexl.h \
+  minisat/core/SolverTypes.h minisat/mtl/Alg.h minisat/mtl/Vec.h \
+  minisat/mtl/XAlloc.h minisat/mtl/IntMap.h minisat/mtl/Map.h \
+  minisat/mtl/Alloc.h maxhs/core/MaxSolverTypes.h maxhs/core/Bvars.h \
+  maxhs/ifaces/SatSolver.h minisat/core/Solver.h minisat/mtl/Heap.h \
+  minisat/utils/Options.h minisat/utils/ParseUtils.h maxhs/utils/io.h \
+  maxhs/ds/Packed.h maxhs/core/Wcnf.h maxhs/ifaces/miniSatSolver.h \
+  minisat/simp/SimpSolver.h minisat/mtl/Queue.h maxhs/utils/Params.h
